0f44f4d1033c129592c75067b60bdccfde75af0c
Anthony Tran <quanghust96@gmail.com>
2022-11-26 11:48:53 +0700

    Code gan hoan thien

--- casio.js
+++ casio.js
@@ -4,26 +4,27 @@
   let operator=document.querySelector(".operator");
   let number=document.querySelector(".number");
   let equal=document.querySelector(".equal");
-  // console.log(display);
-  // console.log(operator);
-  // console.log(number);
-  // console.log(equal);
+  let check=false;
+  
   function numberDisplay(node){
     node.childNodes.forEach(element => {
     if (element.nodeName!=="#text") {
       element.onclick=function(){
+        if (check===true) display.innerText="";
         if (element.innerText==="C") display.innerText=null; 
         else
         if((element.innerText===".")&&(display.innerText[display.innerText.length-1]===".")) return;
           else display.innerText=display.innerText +element.innerText;
+        check=false;
       }
-  }});
+    }});
   };
 
   function operatorDisplay(node){
     node.childNodes.forEach(element => {
       if (element.nodeName!=="#text") {
         element.onclick=function(){
+            if (check===true) display.innerText="";
             switch(display.innerText[display.innerText.length-1]){
               case "+":
               case "-":
@@ -35,17 +36,30 @@
                 }
               default:  display.innerText=display.innerText +element.innerText;
             }
+            check=false;
         }
     }});
   };
 
   function Equal(){
     equal.onclick=function(){
-      console.log(Mathdisplay.innerText));
+      switch(display.innerText[display.innerText.length-1]){
+        case "+":
+        case "-":
+        case "*":
+        case "/": {display.innerText="ERROR!!"; break;}
+        default: display.innerText=parse(display.innerText);
+      }
+      check=true; 
     }
   }
 
+  function parse(inputString){
+    let result= new Function (`return (${inputString})`);
+    return result();
+  };
+  Equal(); 
   operatorDisplay(operator);
   numberDisplay(number);
-  Equal();
+ 
  }
